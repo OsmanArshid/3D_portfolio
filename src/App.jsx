43475c589a98d2ba8aca9111dc6d97ef1e270cad
OsmanArshid <usman.arshid01@outlook.com>
2025-06-10 00:56:28 +0500

    Setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+// import { Footer, Navbar } from "./components";
+// import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-small underline'>hello</h1>
-    </div>
-  )
-}
 
-export default App
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello bro</h1>
+    </div>
+    // <main className='bg-slate-300/20'>
+    //   <Router>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path='/' element={<Home />} />
+    //       <Route
+    //         path='/*'
+    //         element={
+    //           <>
+    //             <Routes>
+    //               <Route path='/about' element={<About />} />
+    //               <Route path='/projects' element={<Projects />} />
+    //               <Route path='/contact' element={<Contact />} />
+    //             </Routes>
+    //             <Footer />
+    //           </>
+    //         }
+    //       />
+    //     </Routes>
+    //   </Router>
+    // </main>
+  );
+};
+
+export default App;
