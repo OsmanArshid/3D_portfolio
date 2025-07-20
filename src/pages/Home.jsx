7c397dfca38f72003c9f52775344dee255a46d0a
OsmanArshid <usman.arshid01@outlook.com>
2025-07-20 18:10:13 +0500

    continue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
+{/* <div className = 'absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+  POPUP
+</div> */}
+import { Canvas } from "@react-three/fiber"; 
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className = 'w-full h-screen relative'>
+      {/* Canvas is the ROOT component that sets up our entire 3D scene... 
+      ALL 3D will be rendered here (the "Renderer" in the guide doc) */}
+      <Canvas>
+
+      </Canvas>
+    </section>
   )
 }
 
