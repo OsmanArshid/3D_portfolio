789dfd7a46a1af963b83a8dbeb68aba58b9310c5
OsmanArshid <usman.arshid01@outlook.com>
2025-07-27 00:26:47 +0500

    Update Island.jsx

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -17,11 +17,12 @@ import { use } from 'react';
 
 const Island = (isRotating, setIsRotating, ...props) => {
   const islandRef = useRef();
+
   const { nodes, materials } = useGLTF(IslandScene)
   
   
   // now we have to get access to 3js rendered and viewport
-  const {gl, viewport} = useThree(); // this is a hook\
+  const {gl, viewport} = useThree(); // this is a hook
 
 
   // now we use a ref to get last mouse x-position 
@@ -54,31 +55,133 @@ const Island = (isRotating, setIsRotating, ...props) => {
     e.stopPropagation();
     e.preventDefault(); 
     setIsRotating(false);
-
-    // so now we will check if the user is on screen or PC using a mouse
-    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
-
-    // checking the change in horizontal pos
-    const delta = (clientX - lastX.current) / viewport.width;
-
-    // we wanna update the island's rotation based on the mouse
-    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-
-    // Update the reference for the last clientX position
-    lastX.current = clientX;
-
-    // Update the rotation speed
-    rotationSpeed.current = delta * 0.01 * Math.PI;
   }
 
   const handlePointerMove = (e) => {
     e.stopPropagation();
     e.preventDefault();
 
-    if (isRotating) handlePointerUp(e);
+    if (isRotating)
+    {
+      // so now we will check if the user is on screen or PC using a mouse
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+      // checking the change in horizontal pos
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      // we wanna update the island's rotation based on the mouse
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+      // Update the reference for the last clientX position
+      lastX.current = clientX;
+
+      // Update the rotation speed
+      rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
   }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowLeft")
+    {
+      if (!isRotating) setIsRotating(true);
+
+      islandRef.current.rotation.y += 0.01 * Math.PI; // --------------- CHECK LATER --------------
+    }
+
+    if (e.key === "ArrowRight")
+    {
+      if (!isRotating) setIsRotating(true);
+
+      islandRef.current.rotation.y -= 0.01 * Math.PI;  // --------------- CHECK LATER --------------
+    }
+  }
+
+  const handleKeyUp = (e) => {
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight")
+    {
+      setIsRotating(false);
+    }
+  }
+
 // ===============================================================================================================
 
+  // a lot of these things wont change (functions), but its good practice to use them
+  useEffect(() => {
+    // we're not touching the regular dom, we're touching the 3js rendered dom
+    const canvas = gl.domElement; 
+
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    canvas.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+    
+    return () => {
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+      canvas.removeEventListener('pointerup', handlePointerUp);
+      canvas.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  
+  }, [gl,handlePointerDown,handlePointerUp,handlePointerMove]);
+
+// ===============================================================================================================
+
+  // this is a hook that runs every frame of the app 
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) { // if the speed becomes less than this, its gonna stop
+        rotationSpeed.current = 0;
+      }
+    } 
+    
+    else {
+      // When rotating, determine the current stage based on island's orientation
+      const rotation = islandRef.current.rotation.y;
+
+      /**
+       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
+       * The goal is to ensure that the rotation value remains within a specific range to
+       * prevent potential issues with very large or negative rotation values.
+       *  Here's a step-by-step explanation of what this code does:
+       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
+       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
+       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
+       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
+       *     This is done to ensure that the value remains positive and within the range of
+       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
+       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
+       *     modulo operation to the value obtained in step 2. This step guarantees that the value
+       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
+       *     circle in radians.
+       */
+      const normalizedRotation = ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation, and stop at different stages
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
+
+// ===============================================================================================================
+// ==============================================================================================================
 
   return (
     
